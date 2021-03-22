b7c1dd9a73d90930df534a7fbca90017e3e37cb2
mayank <maykat2017@gmail.com>
2021-03-22 16:16:09 +0530

    csv_updated

--- script.py
+++ script.py
@@ -2,8 +2,12 @@ import subprocess
 import wget
 
 a = '3S-A new benchmark for Knowledge Augmented VQA results.csv'
+
 wget.download('https://competitions.codalab.org/competitions/29930/results/48980/data')
 subprocess.run(['mv', a, 'okvqa.csv'])
+
+wget.download('https://competitions.codalab.org/competitions/29930/results/48981/data')
+subprocess.run(['mv', a, 's3vqa.csv'])
 
 subprocess.run(["git", 'add', '.'])
 subprocess.run(["git", 'commit', '-m', 'csv_updated'])
